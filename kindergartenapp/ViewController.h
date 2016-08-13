6b5f6a365486c925fdbc95102e78816503d74f30
baniasthbird <2002-sunshine@163.com>
2016-08-13 18:52:46 +0800

    Initial Commit

---
+++ kindergartenapp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  kindergartenapp
+//
+//  Created by zr-mac on 16/8/13.
+//  Copyright © 2016年 zr-mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
