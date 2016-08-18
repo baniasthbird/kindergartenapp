66bf53290d5fd93570a5befb0f32948fee7c6dd1
baniasthbird <2002-sunshine@163.com>
2016-08-18 23:20:09 +0800

    0818  完善登陆页面设计

--- kindergartenapp/ViewController.m
+++ kindergartenapp/ViewController.m
@@ -10,6 +10,10 @@
 
 @interface ViewController ()
 @property (weak, nonatomic) IBOutlet UIButton *login_Parent;
+@property (weak, nonatomic) IBOutlet UIView *bgView;
+@property (weak, nonatomic) IBOutlet UITextField *txtUsrName;
+@property (weak, nonatomic) IBOutlet UITextField *txtPwd;
+@property (weak, nonatomic) IBOutlet UIImageView *imgarrow;
 
 
 @end
@@ -24,10 +28,20 @@
    
     _login_Parent.layer.borderColor=[[UIColor colorWithRed:187/255.0f green:187/255.0f blue:187/255.0f alpha:1] CGColor];
     _login_Parent.backgroundColor=[UIColor colorWithRed:243/255.0f green:131/255.0f blue:45/255.0f alpha:1];
-    _login_Parent.layer.cornerRadius=5;
+    _login_Parent.layer.cornerRadius=10;
+    [_login_Parent addTarget:self action:@selector(Login:) forControlEvents:UIControlEventTouchUpInside];
     [_login_Parent setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
-    
+    [_txtUsrName setBorderStyle:UITextBorderStyleNone];
+    [_txtPwd setBorderStyle:UITextBorderStyleNone];
+    _bgView.layer.cornerRadius=10;
+   
+    [_imgarrow setHidden:YES];
 
+    
+}
+
+//登陆
+-(void)Login:(UIButton*)sender {
     
 }
 
